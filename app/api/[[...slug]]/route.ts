fb4066489bdbe82a3f2e8ab8c322f9359c4080f8
Matias Lopez <matychp@gmail.com>
2024-04-21 17:50:34 -0300

    :sparkles: Add elysia

---
+++ app/api/[[...slug]]/route.ts
@@ -0,0 +1,12 @@
+import { Elysia, t } from "elysia";
+
+const app = new Elysia({ prefix: "/api" })
+  .get("/", () => "hello Next")
+  .post("/", ({ body }) => body, {
+    body: t.Object({
+      name: t.String(),
+    }),
+  });
+
+export const GET = app.handle;
+export const POST = app.handle;
--- app/api/route.ts
+++
@@ -1,6 +0,0 @@
-export const dynamic = "auto"; // defaults to auto
-export async function GET(request: Request) {
-  return Response.json({
-    hello: "world",
-  });
-}
